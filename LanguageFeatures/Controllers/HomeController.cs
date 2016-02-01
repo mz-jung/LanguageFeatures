570d55794faffe6cab8cb687783e2cfb50314999
mz-jung <jung@mz.co.kr>
2016-02-01 19:15:34 +0900

    4장 100page

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using LanguageFeatures.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "Navigate to a URL to show an example";
+        }
+
+        public ViewResult AutoProperty() {
+            Product myProduct = new Product();
+
+            myProduct.Name = "kim";
+
+            string productName = myProduct.Name;
+
+            return View("Result", (object)String.Format("Product name : {0}", productName));
+        }
+
+        public ViewResult CreateProduct() {
+            // 개체 이니셜라이저 기능 
+            Product myProduct = new Product
+            {
+                ProductId = 1,
+                Name = "Lee",
+                Description = "hi~",
+                Price = 200M,
+                Category = "waterSports"
+            };
+
+            return View("Result", (object)String.Format("Category: {0}", myProduct.Category));
+        }
+
+        public ViewResult CreateCollection() {
+            string[] stringArray = { "apple", "orange", "plum" };
+            List<int> intList = new List<int> { 10, 20, 30, 40 };
+
+            Dictionary<string, int> myDict = new Dictionary<string, int> {
+                {"apple",10 } , {"orange", 20}, {"plum", 30 }
+            };
+
+            return View("Result", (object)stringArray[1]);
+        }
+    }
+}
---
+++ LanguageFeatures/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LanguageFeatures.Models
+{
+    public class Product
+    {
+        
+        /// <summary>
+        /// 자동으로 구현된 속성기능 사용
+        /// </summary>
+        /// 
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+
+
+    }
+}
